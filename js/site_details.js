b692a009ee4fbf535fea38dfd410e6edfec2cd7e
Ksites <158746568+gprogramming360@users.noreply.github.com>
2024-05-29 07:23:58 +0100

    update site_details.js

--- js/site_details.js
+++ js/site_details.js
@@ -3,3 +3,6 @@ var anime_name="Demon Slayer";
 var site_logo="../img/logo.webp";
 var site_image="../img/background-1.webp";
 var site_url="https://reading-manga-online360.on.drv.tw/kimetsu/";
+//ONLY FOR MANGA KEYWORDS
+
+//END ONLY FOR MANGA KEYWORDS
