2430dad627ff92eac6ed39d8e8a5ba5038edfae5
Ksites <158746568+gprogramming360@users.noreply.github.com>
2024-05-29 07:16:32 +0100

    update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,1 +1,22 @@
 
+//FOOTER
+var b="<footer id='footer'>";
+document.write(b);
+
+var c="<div class='block-1'><a href='../index.html' alt='home'>Home</a></br><a href='#manga' alt='manga'>Manga</a></br><a href='#seasons' alt='episode'>Episode</a></br><a href='sitemap.txt' alt='sitemap'>Sitemap</a></div>";
+document.write(c);
+
+var d="<div class='block-2'><a href='https://www.facebook.com/onepiece360' target='_blank' alt='facebook'>Facebook </a><a href='https://m.youtube.com/@onepiece-plus' target='_blank' alt='youtube'>Youtube </a><a href='https://onepiece-plus.com.ng' target='_blank' alt='website'>Website</a></div>";
+document.write(d);
+
+var e="<div class='block-3'><span>Copyright Onepiece plus. All rights reserved.  This site is updated, watch episodes and read High Quality manga free. <b> Visit our official website at <a href='https://onepiece-plus.com.ng' target='_blank' alt='Onepiece plus'>Onepiece plus</a></b></span></div>";
+document.write(e);
+
+var b="</footer>";
+document.write(b);
+//END FOOTER
+
+//MISCELLANEOUS CODE
+var f="<div class='apk'><a href='../kimetsu.apk' alt='Download Kimetsu Official App'><img src='../img/apk.webp' alt='apk logo'></a></div>";
+document.write(f);
+//END MISCELLANEOUS CODE
