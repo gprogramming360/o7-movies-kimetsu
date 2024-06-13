146fefe3b586b6769fb650cf34af976398787875
Ksites <158746568+gprogramming360@users.noreply.github.com>
2024-06-13 13:16:33 +0100

    update vignetteads.js

--- vignetteads.js
+++
@@ -1,2 +0,0 @@
-var ads2="<script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('goomaphy.com',7608133,document.createElement('script'))</script>";
-document.write(ads2);
