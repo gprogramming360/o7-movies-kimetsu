6bfcf03d92c6c18b66035f39ce96e572da351415
Ksites <158746568+gprogramming360@users.noreply.github.com>
2024-06-13 13:07:53 +0100

    update vignetteads.js

--- vignetteads.js
+++ vignetteads.js
@@ -1,2 +1,2 @@
-var ads2="<script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://zovidree.com/tag.min.js',7608120,document.body||document.documentElement)</script>";
+var ads2="<script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('goomaphy.com',7608133,document.createElement('script'))</script>";
 document.write(ads2);
