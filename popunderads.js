1c814bba6208f46b20bc889f106444f8be316c8f
Ksites <158746568+gprogramming360@users.noreply.github.com>
2024-06-13 12:53:44 +0100

    update popunderads.js

--- popunderads.js
+++ popunderads.js
@@ -1,1 +1,1 @@
-<script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://zovidree.com/tag.min.js',7608120,document.body||document.documentElement)</script>
+(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://zovidree.com/tag.min.js',7608120,document.body||document.documentElement)
