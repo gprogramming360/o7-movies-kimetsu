4d33770deeaca4d9313725f896486b2d88d89307
Ksites <158746568+gprogramming360@users.noreply.github.com>
2024-06-21 15:10:45 +0100

    update site_details.js

--- js/site_details.js
+++ js/site_details.js
@@ -3,6 +3,7 @@ var anime_name="Demon Slayer";
 var site_logo="../img/logo.webp";
 var site_image="../img/background-1.webp";
 var site_url="https://demon-slayer.watchfilm.xyz";
+var manga_url="https://readingmanga-nonstop360.on.drv.tw";
 //ONLY FOR MANGA KEYWORDS
 var keywords="Demon slayer, demon slayer anime series, demon slayer manga online, read demon slayer manga online, read demon slayer latest chapter online, download demon slayer movies, download demon slayer anime, download demon slayer movies HD, Download demon slayer latest anime series, demon slayer hashira training, demon slayer season 1, demon slayer season 2,demon slayer season 3,demon slayer season 4,demon slayer season 5, kimetsu no yaiba, kimetsu 360";
 //END ONLY FOR MANGA KEYWORDS
